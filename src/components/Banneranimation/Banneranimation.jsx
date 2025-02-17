ccc5376af11492f0302cc8f6e8914583c22d2a3f
harish <harish.haridas@dgverse.in>
2025-02-18 01:36:59 +0530

    added file

--- src/components/Banneranimation/Banneranimation.jsx
+++ src/components/Banneranimation/Banneranimation.jsx
@@ -1,4 +1,9 @@
-import { useEffect, useState } from "react";
+/* eslint-disable */
+import { useEffect, useState } from 'react';
+import { Box, Typography, Grid, Button, useMediaQuery, CardMedia } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+// import { Link } from 'react-router-dom';
+
 import centerPic from "../../Assets/center.png";
 import "./Banneranimation.scss";
 
@@ -25,6 +30,9 @@ const imagesRight = [
 ];
 
 const Banneranimation = () => {
+  const theme = useTheme();
+  const matchesMd = useMediaQuery(theme.breakpoints.up(1024));
+
   const [radius, setRadius] = useState(265); // Default radius
 
   // Function to update radius based on screen width
@@ -48,7 +56,49 @@ const Banneranimation = () => {
   }, []);
 
   return (
-    <div className="banner-animation">
+    <Grid container direction={matchesMd ? 'row' : 'column'}
+    sx={{ minHeight: { xs: '500px', md: '500px', lg: '500px' } }}
+    justifyContent={'center'} wrap='nowrap' className="banner-animation">
+      <Grid container justifyContent="center" sx={{ pl: {lg: '50px'}, p: { md: '10px', lg: '0px' } }} direction="column">
+        <Typography
+          // variant={matchesMd ? 'h2' : 'h4'}
+          fontSize={{ xs: '2rem', md: '3rem' }}
+          textAlign={matchesMd ? 'left' : 'center'}
+          className="background-black-gradient"
+          fontFamily="Raleway"
+          fontWeight={600}
+          sx={{
+            [theme.breakpoints.up('lg')]: {
+              textWrap: 'nowrap',
+            },
+          }}
+          gutterBottom
+        >
+          The Future of Credentialing
+        </Typography>
+        <Typography
+          variant={matchesMd ? 'h4' : 'h6'}
+          textAlign={matchesMd ? 'left' : 'center'}
+          className="background-black-gradient"
+          fontFamily="Raleway"
+          gutterBottom
+        >
+          Cleaner.Greener.Faster
+        </Typography>
+        <Box sx={{ display: 'flex', gap: 2, justifyContent: matchesMd ? 'left' : 'center', mt: 2 }}>
+          {/* <Link to="/profile/add"> */}
+          <Button variant="contained" color="secondary" sx={{ color: 'black' }}>
+            Free Trial
+          </Button>
+          {/* </Link> */}
+          {/* <Button variant="outlined" color="secondary">
+                  Login
+                </Button> */}
+          {/* <Button variant="outlined" onClick={handleSignInModal}>
+                  <Typography>Login</Typography>
+                </Button> */}
+        </Box>
+      </Grid>
     <div className="outer-part">
       <div className="side-outer">
         <div className="hole">
@@ -105,7 +155,7 @@ const Banneranimation = () => {
         </div>
       </div>
     </div>
-    </div>
+    </Grid>
   );
 };
 
